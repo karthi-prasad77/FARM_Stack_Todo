5fab4112a7a7649258a84082eae93396b14b6674
karthi-prasad77 <karthiprasad44@gmail.com>
2023-01-27 11:43:56 +0530

    Create methods

---
+++ Backend/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    remove_todo
+)
+
+# create an Fast API object
+app = FastAPI()
+
+# create middleware to allow react to contact the backend
+origins = ['https://localhost:3000']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Ping": "Pong"}
+
+@app.get("/api/todo")
+async def get_todo():
+    response = await fetch_all_todos()
+    return response
+
+@app.get("/api/todo{title}", response_model = Todo)
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo item with this title {title}")
+
+@app.post("/api/todo/", response_model = Todo)
+async def post_todo(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something wenr wrong")
+
+@app.put("/api/todo{title}/", response_model = Todo)
+async def put_todo(title:str, desc:str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There was no todo item with this title {title}")
+
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully removed the todo item !"
+    raise HTTPException(404, f"There was no todo item with this title {title} to remove from the database")
