19dccefa119dd2aff064a426ad0a63436184a485
karthi-prasad77 <karthiprasad44@gmail.com>
2023-01-27 11:43:18 +0530

    MongoDb connection

---
+++ Backend/database.py
@@ -0,0 +1,37 @@
+from model import Todo
+# mongodb driver
+import motor.motor_asyncio as db
+
+# mongodb connection
+client = db.AsyncIOMotorClient("mongodb://localhost:27017")
+
+# create database
+database = client.TodoList
+
+# create collections
+collection = database.todo
+
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title":title})
+    return document
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+    async for document in cursor:
+        todos.append(Todo(**document))
+    return todos
+
+async def create_todo(todo):
+    document = todo
+    result = await collection.insert_one(document)
+    return document
+
+async def update_todo(title, desc):
+    await collection.update_one({"title":title}, {"$set":{"description": desc}})
+    document = await collection.find_one({"title":title})
+    return document
+
+async def remove_todo(title):
+    await collection.delete_one({"title":title})
+    return True
